1f4fd6755a53cdff902b720cc8e9477ad5ba6503
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-17 22:43:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,17 +1,11 @@
+#def square_array(array)
+  # your code here
+#end
+
 def square_array(array)
-  # your code here	
-
-
-#create my new array	#create my new array to this assignment
-myArray = []	my_array = []
-
- n an element that stores values in the pipes
-array.each do |n|	array.each do |n|
-    my_value = n**2 # this to square n
-my_array << my_value
-end #end loop	end #end loop
-
-
-# call my array	# call my array
-myArray	my_array
-end	
+  new_numbers = []
+  array.each do |number|
+    new_numbers << number * number
+  end
+  return new_numbers
+end
