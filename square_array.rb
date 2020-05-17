dcd0b3da9e37b9781a5b7b0e702362169789ac00
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-17 22:42:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -8,3 +8,10 @@ myArray = []	my_array = []
  n an element that stores values in the pipes
 array.each do |n|	array.each do |n|
     my_value = n**2 # this to square n
+my_array << my_value
+end #end loop	end #end loop
+
+
+# call my array	# call my array
+myArray	my_array
+end	
